f7e9dffb6838edbf40061b761ef1682ff49159f1
Kervin Gonzalez <kervingonzalez@gmail.com>
2022-06-27 16:53:23 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { engine } from 'express-handlebars';
+import hbs from 'hbs'
+import dotenv from 'dotenv/config'
+
+const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT
+
+
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(app.get('views')),
+  defaultLayout: 'home',
+}));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Middleware
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: 'Kervin Gonzalez',
+    title: 'Curso node'
+  })
+})
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    layout: false,
+    name: 'Kervin Gonzalez',
+    title: 'Curso node'
+  })
+
+})
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    layout: false,
+    name: 'Kervin Gonzalez',
+    title: 'Curso node'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(port)
